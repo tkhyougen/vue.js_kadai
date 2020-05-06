abed78f0d09aae591b26408d4f8127f171db19b3
tkhyougen <illgalsubstance@yahoo.co.jp>
2020-05-06 14:03:09 +0000

    課題提出

---
+++ kadai.js
@@ -0,0 +1,21 @@
+
+new Vue({
+  el: '#app',
+  data: {
+    name: '',
+    course: '',
+    acceptancePeriod: '',
+    defaultLastId: 3,
+    students: [
+      { id: 1, name: '野呂浩良', course: '機械学習コース', acceptancePeriod: '2019年01月期' },
+      { id: 2, name: '富永修司', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' },
+      { id: 3, name: '斉藤一起', course: 'Webエンジニアコース', acceptancePeriod: '2017年11月期' }
+    ]
+  },
+  methods: {
+    addStudent: function() {
+      this.students.push({ name: this.name, course: this.course,
+        acceptancePeriod: this.acceptancePeriod, id: this.defaultLastId += 1})
+    }
+  }
+})
